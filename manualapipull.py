46fe71e31d1b50d2590e53394d7e398a89d233bc
ngalantowicz <ngalantowicz@gmail.com>
2017-06-18 12:12:20 -0400

    added unwind aggregate mongo fn

--- manualapipull.py
+++ manualapipull.py
@@ -2,19 +2,19 @@ from ohmysportsfeedspy import MySportsFeeds
 from api_access import username, password
 import json
 from playerlog_fns import playerlog_apipull, playerlog_reformat
-#from mongo_fns import mongoimport, mongoread, mongoremove
+from mongo_fns import mongoimport, mongoread, mongoremove, mongounwind
 
 
 # Comment out  all three lines \/\/\/ when reading from mongo
-#Data_query = MySportsFeeds('1.0',verbose=True)
-#Data_query.authenticate(username, password)
+# Data_query = MySportsFeeds('1.0',verbose=True)
+# Data_query.authenticate(username, password)
 
 #Example: #Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='player_gamelogs',format='json',player='stephen-curry')
 
 ##API Documentation: https://www.mysportsfeeds.com/data-feeds/api-docs/
 
-#playergamelog= Data_query.msf_get_data(league='nhl', season='2016-2017-regular', feed='player_gamelogs',format='json', team = 'cbj', player = 'zach-werenski,'cam-atkinson')
-
+# playergamelog= Data_query.msf_get_data(league='nhl', season='2016-2017-regular', feed='player_gamelogs',format='json', team = 'cbj', player = 'zach-werenski, cam-atkinson', limit = '100')
+# playergamelog = playergamelog["playergamelogs"]
 #teamgamelog = Data_query.msf_get_data(league='nhl',season='2016-2017-regular',feed='team_gamelogs', format='json')
 
 #draftking = Data_query.msf_get_data(league='nhl',season='2016-2017-regular', feed='daily_dfs', format='json', team = 'cbj',player='zach-werenski')
@@ -25,22 +25,22 @@ from playerlog_fns import playerlog_apipull, playerlog_reformat
 #print(json.dumps(draftking, indent=4))
 
 # Comment out \/\/\/ if you have data inserted and only want to read
-#Data_dbImport = mongoimport(Output)
+# Data_dbImport = mongoimport(playergamelog)
 
 # Comment out \/\/\/ if you want to read newly insert data
-#Data_dbRead = mongoread()
+Data_dbRead = mongounwind()
 
 # Comment out \/\/\/ if you dont want to delete data
 #Data_Remove = mongoremove()
 
 #To pull player logs for teams
-teamnames = ['cbj','det']
+# teamnames = ['cbj','det']
 
-playerlogoutput = []
-for team in teamnames:
-    data = playerlog_apipull(team)
-    reformat = playerlog_reformat(data)
-    playerlogoutput += reformat
-
-with open('playerlogoutput', 'w') as fout:
-    json.dump(playerlogoutput, fout)
+# playerlogoutput = []
+# for team in teamnames:
+#     data = playerlog_apipull(team)
+#     reformat = playerlog_reformat(data)
+#     playerlogoutput += reformat
+#
+# with open('playerlogoutput', 'w') as fout:
+#     json.dump(playerlogoutput, fout)
--- mongo_fns.py
+++ mongo_fns.py
@@ -3,17 +3,28 @@ from pymongo import MongoClient
 def mongoimport(data):
     client = MongoClient('localhost', 27017)
     db = client.nhl_ml
-    coll = db.first_test
+    coll = db.player_logs
     insert = coll.insert_one(data)
     print "Data imported into mongodb 'nhl_ml' collection with ID of {}".format(insert.inserted_id)
 
 def mongoread():
     client = MongoClient('localhost', 27017)
     db = client.nhl_ml
-    coll = db.first_test
-    cursor = coll.find({"teamgamelogs.gamelogs.game.homeTeam.Abbreviation": "CBJ"})
+    coll = db.player_logs
+    cursor = coll.find({"gamelogs.player.LastName" : "Werenski"})
     for document in cursor:
         print(document)
+
+def mongounwind():
+    client = MongoClient('localhost', 27017)
+    db = client.nhl_ml
+    coll = db.player_logs
+    cursor = coll.aggregate( [
+    { "$unwind": "$gamelogs" }
+    ] )
+    for document in cursor:
+        print(document)
+        print()
 
 def mongoremove():
     client = MongoClient('localhost', 27017)
